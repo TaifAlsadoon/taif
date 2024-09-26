c5ae03a8b89db4c04f1af248dbb253c7974b2cf3
TAIF <taif-alsadoon@outlook.com>
2024-09-26 17:13:09 +0300

    Initial Commit

---
+++ taif/taifApp.swift
@@ -0,0 +1,17 @@
+//
+//  taifApp.swift
+//  taif
+//
+//  Created by Taif Alsadoon on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct taifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taifTests/taifTests.swift
@@ -0,0 +1,17 @@
+//
+//  taifTests.swift
+//  taifTests
+//
+//  Created by Taif Alsadoon on 23/03/1446 AH.
+//
+
+import Testing
+@testable import taif
+
+struct taifTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
